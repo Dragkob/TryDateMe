857d0608f3b1a569a25f1f5610252a9608750404
DKob <141576033+Dragkob@users.noreply.github.com>
2026-01-31 01:04:59 +0100

    Add files via upload

---
+++ TryDateMe/js/background.js
@@ -0,0 +1,14 @@
+//  storage operations
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "getLocalStorage") {
+    chrome.storage.local.get(request.keys, (result) => {
+      sendResponse(result);
+    });
+    return true;
+  } else if (request.action === "setLocalStorage") {
+    chrome.storage.local.set(request.data, () => {
+      sendResponse({ success: true });
+    });
+    return true; 
+  }
+});
